7c04b7465238929e46fce706422742ae041b384a
Vishwa <venkatesh.koolla@gmail.com>
2020-05-06 11:41:12 -0400

    Change to Production in appsettings

--- Data/Identity/ApplicationUser.cs
+++ Data/Identity/ApplicationUser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 
-
 namespace IdentityServer.Data.Identity
 {
     public class ApplicationUser : IdentityUser
